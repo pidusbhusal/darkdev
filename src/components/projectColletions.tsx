70a9d2ea19c71f442872cd966bf7f63a3bc23068
Sudip Bhusal <pidusbhusal@gmail.com>
2023-05-10 13:44:39 +0545

    image compressed

--- src/components/projectColletions.tsx
+++ src/components/projectColletions.tsx
@@ -6,7 +6,7 @@ const projects = [
         name: 'Sunrise E-commerce',
         description: " Mobile App design for Ecommerce Company which sells wide range of products. I was tasked with designing a interactive mobile app with prototype and interaction animations",
         link: "https://www.behance.net/gallery/156320125/Sunrise-Ecommerce",
-        image: "sunrise.jpg",
+        image: "sunrise.webp",
         message: "Read Case Study"
     },
     {
@@ -14,7 +14,7 @@ const projects = [
         name: 'Portfolio for Full Stack Developer',
         description: "Aashan Ghimire, a Full Stack Developer, approached me with a unique project. He was seeking a website that would showcase his impressive skills and years of experience that he has amassed throughout his career. My task was to create a User-Friendly design, and develop a front-end template with interactive features using Framer.js. Together, we crafted a website that truly captures the essence of Aashan's expertise and passion for his work.",
         link: "https://aashan.dev/",
-        image: "devport.jpg",
+        image: "devport.webp",
         message: "Visit Website"
     },
     {
@@ -22,7 +22,7 @@ const projects = [
         name: 'Logo for Docucontext - AI powered document processing.',
         description: "I was asked to design a logo for - Docucontext, a cloud-native document processing solution powered by AI. With the challenge at hand, I crafted a logo that effectively embodies the essence of Docucontext's purpose and functionalities.",
         link: "https://www.behance.net/gallery/165471227/Document-Scanner-App-Logo",
-        image: "doccontext.jpg",
+        image: "doccontext.webp",
         message: "Read Case Study"
     },
     {
@@ -30,7 +30,7 @@ const projects = [
         name: 'Sunrise E-commerce',
         description: " Mobile App design for Ecommerce Company which sells wide range of products. I was tasked with designing a interactive mobile app with prototype and interaction animations",
         link: "https://www.behance.net/gallery/156320125/Sunrise-Ecommerce",
-        image: "sunrise.jpg",
+        image: "sunrise.webp",
         message: "Under Construction"
     },
 ]
