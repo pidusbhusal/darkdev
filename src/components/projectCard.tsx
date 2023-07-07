7f00d490adc9f961dcabee9954325277a946852a
Sudip Bhusal <pidusbhusal@gmail.com>
2023-07-07 15:30:20 +0545

    isreponsive

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-import Image from 'next/image'
+import Image from 'next/legacy/image';
 import Link from 'next/link'
 import { motion, AnimatePresence } from 'framer-motion'
 
