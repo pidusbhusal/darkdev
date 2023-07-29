f0221bf9e95acab914cb5f20cb282f5596e1eb56
Sudip Bhusal <pidusbhusal@gmail.com>
2023-07-29 22:45:22 +0545

    delayed loading of js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import HotjarLoader from "@/components/HotjarLoader";
 import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
+  HotjarLoader();
   return (
     <>
       <Head>
@@ -67,7 +68,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <NextNProgress color="#254FD9" />
         <Component {...pageProps} />
         <HotjarLoader />
-      
       </Layout>
     </>
   );
